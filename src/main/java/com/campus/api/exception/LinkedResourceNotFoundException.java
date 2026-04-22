46b5504bc12604d12eb4ab90c62451e06b47217d
NadilaBasilu <basilunw@gmail.com>
2026-04-22 22:03:42 +0530

    feat: add LinkedResourceNotFoundException for handling missing linked resources

---
+++ src/main/java/com/campus/api/exception/LinkedResourceNotFoundException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.campus.api.exception;
+
+/**
+ *
+ * @author Basilu
+ */
+
+
+public class LinkedResourceNotFoundException extends RuntimeException {
+    public LinkedResourceNotFoundException(String message) {
+        super(message);
+    }
+}
